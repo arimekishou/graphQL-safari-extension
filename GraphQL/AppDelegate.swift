a3957fca2d86f7cf8fdd823142d8da412b693c9e
Boris Drozdov <boris.drozdov@idftechnology.by>
2025-12-26 12:21:24 +0300

    Initial Commit

---
+++ GraphQL/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  GraphQL
+//
+//  Created by Boris Drozdov on 12/26/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
